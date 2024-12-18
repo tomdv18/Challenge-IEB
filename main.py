a80f8e50d632b147992aab0b7e487d6aae8917e3
tomdv18 <tdellavecchia@fi.uba.ar>
2024-12-18 18:21:55 -0300

    Agrego datos generados

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 #{"name": "BMA", "buy": 105.22, "sell": 105.59, "timestamp": 1734367423776,"source": "B"}
 # 
 
-
-
-
-
 import json
 import logging
+
+ancho_empresa = 10 ## Para imprimir en formato tabla
+ancho_compra = 8
+ancho_venta = 8
+ancho_source = 5
+
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
@@ -42,36 +44,54 @@ def validar_cotizacion(cotizacion):
     return True
 
 
-def procesar_archivo(nombre_archivo):
-    with open(nombre_archivo, "r") as f:
-        for linea in f:
-            try:
-                cotizacion = json.loads(linea)
+def imprimir_ultimas_cotizaciones():
+    logging.info("Ultimas cotizaciones:")
 
-                if validar_cotizacion(cotizacion):
-                    if cotizacion["name"] not in ultimas_cotizaciones:
-                        ultimas_cotizaciones[cotizacion["name"]] = cotizacion
-                    else:
-                        if ultimas_cotizaciones[cotizacion["name"]]["timestamp"] < cotizacion["timestamp"]:
+    for empresa, cotizacion in ultimas_cotizaciones.items():
+        logging.info(f"{empresa:<{ancho_empresa}}\tCompra: {cotizacion['buy']:<{ancho_compra}}  Venta: {cotizacion['sell']:<{ancho_venta}}  Source: {cotizacion['source']:<{ancho_source}}")
+
+
+
+def procesar_archivo(nombre_archivo):
+
+    try:
+        with open(nombre_archivo, 'r') as archivo:
+            try:
+                cotizaciones_archivo = json.load(archivo)  
+                for cotizacion in cotizaciones_archivo:
+                    if validar_cotizacion(cotizacion):
+                       
+                        if cotizacion["name"] not in ultimas_cotizaciones:
                             ultimas_cotizaciones[cotizacion["name"]] = cotizacion
+                        else:
+                            if ultimas_cotizaciones[cotizacion["name"]]["timestamp"] < cotizacion["timestamp"]:
+                                ultimas_cotizaciones[cotizacion["name"]] = cotizacion
                     
-                    cotizaciones.append(cotizacion)
+                        logging.info(f"Cotizacion de empresa {cotizacion["name"]} procesada")
+                        cotizaciones.append(cotizacion)
+
+                logging.info(f"Cotizaciones del archivo {nombre_archivo} procesadas.")
+                imprimir_ultimas_cotizaciones()
+
             except json.JSONDecodeError:
-                logging.error(f"Error al procesar la línea: {linea}")
+                logging.error(f"El archivo {nombre_archivo} no contiene un JSON valido.")
+
+    except FileNotFoundError:
+        logging.error(f"El archivo {nombre_archivo} no fue encontrado.")
+
+
+
             
-    logging.info("Cotizaciones procesadas correctamente.")
-    logging.info("Cotizaciones: %s", ultimas_cotizaciones)
-                    
 
 if __name__ == "__main__":
 
     while True:
         try:
-            input = input("Ingrese el nombre del archivo a procesar (o 'q' para terminar): ").strip()
-            if input.lower() == 'q':
-                logging.info("Ejecución terminada por el usuario.")
+            archivo = input("Ingrese el nombre del archivo a procesar (o 'q' para terminar): ").strip()
+            if archivo.lower() == 'q':
+                logging.info("Ejecucion terminada por el usuario.")
                 break
-            procesar_archivo(input)
+            procesar_archivo(archivo)
         except KeyboardInterrupt:
-            logging.info("Ejecución interrumpida por el usuario.")
+            logging.info("Ejecucion interrumpida por el usuario.")
             break
